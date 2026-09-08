17b482f56c2b19724f953446de4ff96ac2d000a6
Omar Kittaneh <omar.kitt7@gmail.com>
2026-09-08 16:12:48 +0300

    Number To Text

--- Problems/Problem01.cpp
+++ Problems/Problem01.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
+string NumberToText(long long Number) {
+    if (Number == 0) {
+        return "";
+    }
+    if (Number >= 1 && Number <= 19) {
+        string arr[] = {"", "One", "Two", "Three", "Four", "Five", "Six",
+            "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen",
+            "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        return arr[Number] ;
+    }
+    if (Number >= 20 && Number <= 99) {
+        string arr[] = {"", "", "Twenty", "Thirty", "Forty", "Fifty",
+                        "Sixty", "Seventy", "Eighty", "Ninety"};
+        return arr[Number / 10] + " " + NumberToText(Number % 10);
+    }
+    if (Number >= 100 && Number <= 199) {
+        return "One Hundreds " + NumberToText(Number % 100);
+    }
+    if (Number >= 200 && Number <= 999) {
+        return NumberToText(Number / 100) + " Hundreds " + NumberToText(Number % 100);
+    }
+    if (Number >= 1000 && Number <= 1999) {
+        return "One Thousands " + NumberToText(Number % 1000);
+    }
+    if (Number >= 2000 && Number <= 999999) {
+        return NumberToText(Number / 1000) + " Thousands " + NumberToText(Number % 1000);
+    }
+    if (Number >= 1000000 && Number <= 1999999) {
+        return "One Millions " + NumberToText(Number % 1000000);
+    }
+    if (Number >= 2000000 && Number <= 999999999) {
+        return NumberToText(Number / 1000000) + " Millions " + NumberToText(Number % 1000000);
+    }
+    if (Number >= 1000000000 && Number <= 1999999999) {
+        return "One Billions " + NumberToText(Number % 1000000000);
+    }
+    if (Number >= 2000000000) {
+        return NumberToText(Number / 1000000000) + " Billions " + NumberToText(Number % 1000000000);
+    }
+    return "";
+}
+
+long long ReadNumber() {
+    long long number;
+    cout << "Enter a Number? ";
+    cin >> number;
+    return number;
+}
+
 int main() {
-    cout << "fsaf";
+    cout << NumberToText(ReadNumber()) << endl;
     return 0;
 }
