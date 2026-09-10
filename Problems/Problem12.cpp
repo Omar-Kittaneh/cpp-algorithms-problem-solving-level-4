93eff37123f1c95a6b93b18cfbedc9491bd720e3
Omar Kittaneh <omar.kitt7@gmail.com>
2026-09-10 14:22:28 +0300

    Date Add Days

--- Problems/Problem12.cpp
+++ Problems/Problem12.cpp
@@ -1,9 +1,118 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
+bool IsLeapYear(short Year) {
+    return (Year % 4 == 0 && Year % 100 != 0) || Year % 400 == 0;
+}
+
+short NumberOfDaysInAMonth(short Month, short Year)
+{
+    if (Month < 1 || Month>12)
+        return 0;
+    int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) :
+    days[Month - 1];
+}
+
+short NumberOfDaysFromTheBeginingOfTheYear(short Day, short Month, short Year)
+{
+    short TotalDays = 0;
+    for (int i = 1; i <= Month - 1; i++)
+    {
+        TotalDays += NumberOfDaysInAMonth(i, Year);
+    }
+    TotalDays += Day;
+    return TotalDays;
+}
+
+struct sDate
+{
+    short Year;
+    short Month;
+    short Day;
+};
+
+sDate DateAddDays(int Days, sDate Date)
+{
+    int RemainingDays = Days + NumberOfDaysFromTheBeginingOfTheYear(Date.Day, Date.Month, Date.Year);
+
+    Date.Month = 1;
+    Date.Day = 0;
+
+    while (true) {
+        short DaysInCurrentYear = IsLeapYear(Date.Year) ? 366 : 365;
+        if (RemainingDays > DaysInCurrentYear) {
+            RemainingDays -= DaysInCurrentYear;
+            Date.Year++;
+        }
+        else {
+            break;
+        }
+    }
+
+    while (true) {
+        short MonthDays = NumberOfDaysInAMonth(Date.Month, Date.Year);
+        if (RemainingDays > MonthDays) {
+            RemainingDays -= MonthDays;
+            Date.Month++;
+        }
+        else {
+            break;
+        }
+    }
+
+    Date.Day = RemainingDays;
+    return Date;
+}
+
+
+short ReadDay()
+{
+    short Day;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+    return Day;
+}
+
+short ReadMonth()
+{
+    short Month;
+    cout << "\nPlease enter a Month? ";
+    cin >> Month;
+    return Month;
+}
+
+short ReadYear()
+{
+    short Year;
+    cout << "\nPlease enter a year? ";
+    cin >> Year;
+    return Year;
+}
+
+sDate ReadFullDate()
+{
+    sDate Date;
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+    return Date;
+}
+
+short ReadDaysToAdd()
+{
+    short Days;
+    cout << "\nHow many days to add? ";
+    cin >> Days;
+    return Days;
+}
+
 int main() {
-    
+    sDate Date = ReadFullDate();
+    short Days = ReadDaysToAdd();
+    Date = DateAddDays(Days, Date);
+    cout << "\nDate after adding [" << Days << "] days is: ";
+    cout << Date.Day << "/" << Date.Month << "/" << Date.Year;
     return 0;
 }
