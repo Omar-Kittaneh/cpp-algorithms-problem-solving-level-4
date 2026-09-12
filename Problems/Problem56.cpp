9aa35991eae21fd99202039a4c3cf165f7b64df3
Omar Kittaneh <omar.kitt7@gmail.com>
2026-09-12 22:34:43 +0300

    Is Date1 After Date2

--- Problems/Problem56.cpp
+++ Problems/Problem56.cpp
@@ -1,9 +1,68 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+struct stDate {
+
+    short Day;
+    short Month;
+    short Year;
+};
+
+bool IsDate1BeforeDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year < Date2.Year) ? true : ((Date1.Year ==Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month ==Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+}
+bool IsDate1EqualDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year == Date2.Year) ? ((Date1.Month ==Date2.Month) ? ((Date1.Day == Date2.Day) ? true : false) : false): false;
+}
+bool IsDate1AfterDate2(stDate Date1, stDate Date2)
+{
+    return (!IsDate1BeforeDate2(Date1, Date2) && !IsDate1EqualDate2(Date1, Date2));
+}
+
+short ReadDay()
+{
+    short Day;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+    return Day;
+}
+
+short ReadMonth()
+{
+    short Month;
+    cout << "\nPlease enter a Month? ";
+    cin >> Month;
+    return Month;
+}
+
+short ReadYear()
+{
+    short Year;
+    cout << "\nPlease enter a year? ";
+    cin >> Year;
+    return Year;
+}
+
+stDate ReadFullDate()
+{
+    stDate Date;
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+    return Date;
+}
 
 int main() {
-    
+    cout << "\nEnter Date1:";
+    stDate Date1 = ReadFullDate();
+    cout << "\nEnter Date2:";
+    stDate Date2 = ReadFullDate();
+    if (IsDate1AfterDate2(Date1, Date2))
+        cout << "\nYes, Date1 is After Date2.";
+    else
+        cout << "\nNo, Date1 is NOT After Date2.";
+
     return 0;
 }
