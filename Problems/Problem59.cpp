f49546a2dbd06a77f61a084a6cd62719114a6ef6
Omar Kittaneh <omar.kitt7@gmail.com>
2026-09-13 22:13:12 +0300

    Period Length In Days

--- Problems/Problem59.cpp
+++ Problems/Problem59.cpp
@@ -1,9 +1,146 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+enum enDateCompare{Before = -1 , Equal = 0 , After = 1 };
+struct stDate {
+    short Day;
+    short Month;
+    short Year;
+};
 
+struct stPeriod {
+    stDate StartDate;
+    stDate EndDate;
+};
+
+bool IsLeapYear(short Year) {
+    return (Year % 4 == 0 && Year % 100 != 0) || Year % 400 == 0;
+}
+
+bool IsDate1BeforeDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year < Date2.Year) ? true : ((Date1.Year ==Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month ==Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+}
+
+bool IsDate1EqualDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year == Date2.Year) ? ((Date1.Month ==Date2.Month) ? ((Date1.Day == Date2.Day) ? true : false) : false): false;
+}
+
+bool IsDate1AfterDate2(stDate Date1, stDate Date2)
+{
+    return (!IsDate1BeforeDate2(Date1, Date2) && !IsDate1EqualDate2(Date1, Date2));
+}
+
+enDateCompare CompareDates(stDate Date1, stDate Date2) {
+    if (IsDate1BeforeDate2(Date1, Date2)) {
+        return enDateCompare::Before;
+    }
+    else if (IsDate1AfterDate2(Date1, Date2)) {
+        return enDateCompare::After;
+    }
+    return enDateCompare::Equal;
+}
+
+short NumberOfDaysInMonth(short Month, short Year)
+{
+    if (Month < 1 || Month>12)
+        return 0;
+    int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) :
+    days[Month - 1];
+}
+
+bool IsLastDayInMonth(stDate Date)
+{
+    return (Date.Day == NumberOfDaysInMonth(Date.Month,Date.Year));
+}
+
+bool IsLastMonthInYear(short Month)
+{
+    return (Month == 12);
+}
+
+stDate IncreaseDateByOneDay(stDate Date)
+{
+    if (IsLastDayInMonth(Date))
+    {
+        if (IsLastMonthInYear(Date.Month))
+        {
+            Date.Month = 1;
+            Date.Day = 1;
+            Date.Year++;
+        }
+        else
+        {
+            Date.Day = 1;
+            Date.Month++;
+        }
+    }
+    else
+    {
+        Date.Day++;
+    }
+    return Date;
+}
+
+int GetDifferenceInDays(stDate Date1 , stDate Date2 , bool IncludeEndDay = false) {
+    int Days = 0;
+    while (IsDate1BeforeDate2(Date1, Date2)) {
+        Days++;
+        Date1 = IncreaseDateByOneDay(Date1);
+    }
+    return IncludeEndDay ? ++Days : Days;
+}
+
+int PeriodLengthInDays(stPeriod Period,bool IncludeEndDay = false) {
+    return GetDifferenceInDays(Period.StartDate, Period.EndDate, IncludeEndDay);
+}
+
+short ReadDay()
+{
+    short Day;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+    return Day;
+}
+
+short ReadMonth()
+{
+    short Month;
+    cout << "\nPlease enter a Month? ";
+    cin >> Month;
+    return Month;
+}
+
+short ReadYear()
+{
+    short Year;
+    cout << "\nPlease enter a year? ";
+    cin >> Year;
+    return Year;
+}
+
+stDate ReadFullDate()
+{
+    stDate Date;
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+    return Date;
+}
+
+stPeriod ReadPeriod() {
+    stPeriod Period;
+    Period.StartDate = ReadFullDate();
+    Period.EndDate = ReadFullDate();
+    return Period;
+}
 int main() {
-    
+    cout << "\nEnter Period 1:";
+    stPeriod Period1 = ReadPeriod();
+    cout << "\nPeriod Length is: " << PeriodLengthInDays(Period1);
+    cout << "\nPeriod Length (Including End Date) is: " <<
+    PeriodLengthInDays(Period1, true);
     return 0;
 }
