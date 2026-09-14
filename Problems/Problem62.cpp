46ec2a7f388a70676402af7fda33424d4981ffe9
Omar Kittaneh <omar.kitt7@gmail.com>
2026-09-14 15:40:02 +0300

    Is Valid Date

--- Problems/Problem62.cpp
+++ Problems/Problem62.cpp
@@ -1,9 +1,67 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+struct stDate {
+    short Day;
+    short Month;
+    short Year;
+};
 
+bool IsLeapYear(short Year) {
+    return (Year % 4 == 0 && Year % 100 != 0) || Year % 400 == 0;
+}
+
+short NumberOfDaysInMonth(short Month, short Year)
+{
+    if (Month < 1 || Month>12)
+        return 0;
+    int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) :
+    days[Month - 1];
+}
+
+bool IsValidDate(stDate Date) {
+    if (Date.Month < 1 || Date.Month > 12) return false;
+    return (Date.Day >= 1 && Date.Day <= NumberOfDaysInMonth(Date.Month, Date.Year));
+}
+
+short ReadDay()
+{
+    short Day;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+    return Day;
+}
+
+short ReadMonth()
+{
+    short Month;
+    cout << "\nPlease enter a Month? ";
+    cin >> Month;
+    return Month;
+}
+
+short ReadYear()
+{
+    short Year;
+    cout << "\nPlease enter a year? ";
+    cin >> Year;
+    return Year;
+}
+
+stDate ReadFullDate()
+{
+    stDate Date;
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+    return Date;
+}
 int main() {
-    
+    stDate Date1 = ReadFullDate();
+    if (IsValidDate(Date1))
+        cout << "\nYes, Date is a valide date.\n";
+    else
+        cout << "\nNo, Date is a NOT valide date\n";
     return 0;
 }
